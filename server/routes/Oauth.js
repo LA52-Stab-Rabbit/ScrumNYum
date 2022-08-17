5eb5c3bafe8287f4baaa640a6b63b7588933c6b9
Emma Genesen <emma@Jacobs-MacBook-Pro-2.local>
2022-08-17 14:06:27 -0700

    completed and tested OAuth

--- server/routes/Oauth.js
+++ server/routes/Oauth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 // import controllers 
 
@@ -9,19 +10,66 @@ router.get('/', (req, res) => {
     return res.status(200).redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}`);
 })
 
-router.get('/redirect', async(req, res) => {
+router.get('/redirect', (req, res) => {
    // console.log('request is: ', req)
    //console.log('response is: ', res);
-    const code = req.query;
-   //console.log('code ISSSSSSS', code);
-    const token = await fetch(`https://github.com/login/oauth/access_token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${code}`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
+  const code = req.query.code;
+  console.log('code ISSSSSSS', code);
+  console.log('client_id is: ', process.env.CLIENT_ID)
+  console.log('client_secret is: ', process.env.CLIENT_SECRET)
+  const body = {
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    code,
+  };
+  const opts = { headers: { accept: 'application/json' } };
+
+  axios
+    .post('https://github.com/login/oauth/access_token', body, opts)
+    .then((response) => response.data.access_token)
+    .then((token) => {
+      console.log('My token:', token);
+      res.redirect('/scrum');
     })
-    console.log('token:', token);
+    .catch((err) => {
+      console.log('error in OAuth fetch:', err)
+      res.status(500).json({err: err.message})
+    });
+
+    //ATTEMPT WITH FETCH
+    // URL that we used: ?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${code}
+    // fetch(`https://github.com/login/oauth/access_token`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     // 'Content-Type': 'application/json'
+    //   },
+    //   body: {
+    //     client_id: process.env.CLIENT_ID,
+    //     client_secret: process.env.CLIENT_SECRET,
+    //     code
+    //   }
+    // })
+    //   .then(response => {
+    //     console.log('response before we json', response);
+    //     console.log('response data access token', response.data.access_token);
+    //     response.json()
+    //   })
+    //   .then((data) => {
+    //     console.log('response body from github: ', data);
+    //     // res.redirect(`/?token=${access_token}`)
+    //     res.redirect('/scrum');
+    //   })
+    //   .catch((err) => {
+    //     console.log('error in OAuth fetch:', err);
+    //   })
+      // .then((token) => {
+      //   console.log("TOKEN ISSSSSS: ", token);
+        
+      // })
+  //  const access_token = token.data;
+  //  console.log('token:', access_token);
+   
 })
 
 module.exports = router;
