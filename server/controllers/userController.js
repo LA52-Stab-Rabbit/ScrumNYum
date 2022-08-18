41dbaec31507b0c89d25a30d5bbda2f425f6f2fa
Parker Steinberg <parker@Parkers-MBP-2.lan>
2022-08-18 12:49:27 -0700

    working through GH user middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,28 @@
 const { default: axios } = require('axios');
 const db = require('../models/models.js');
+const { Octokit, App } = require('octokit');
 
 const userController = {};
 
 // verifyUser - Obtain username and password from the request body, locate the appropriate user in the database, and then authenticate the submitted password against the password stored in the database.
 
 userController.createUser = (req, res, next) => {
+  let username, password;
+  // if res.locals.username exists, enter GH user control flow 
+  if (res.locals.githubUsername) {
+    console.log('dealing with Github user in userController.createUser');
+    username = res.locals.githubUsername;
+    password = '';
+  }
+  // else, go through normal create user flow 
+  else {
+    console.log('dealing with NON-Github user in userController.createUser');
+    username = req.body.username;
+    password = req.body.password;
+  }
+
   console.log('in userController.createUser');
-  const { username, password } = req.body;
+  
 
   console.log(req.body);
 
@@ -103,22 +118,53 @@ userController.verifyUser = (req, res, next) => {
     // make axios GET request to github to get GH username
     console.log('running checkGithubUser');
     console.log('token from res.locals is:', res.locals.access_token);
-    // if we can't get GH username, then we can user their email as unique identifier to save to db 
-    const opts = { headers: { 
-      accept: 'application/json',
-      Authorization: 'token' + res.locals.access_token
-      } 
-    };
-    axios
-      .get('https://api.github.com/user')
+    // declare query string to see if user exists in db
+    const query = `
+    SELECT * 
+    FROM users u
+    WHERE u.id = $1  `;
+    // then check their GH username to db (user "github"+ username)
+    
+    // attempting with OKTOKIT (https://github.com/octokit/octokit.js)
+    const octokit = new Octokit({ auth: res.locals.access_token });
+    octokit.request('GET /user', {})
       .then(result => {
-        console.log(result)
-        // return next
-        return next();
-      })
-      .catch(err => {
-        console.log('error in userController.checkGithubUser:', err)
-        return next(err);
+        // attach username to res.locals.username
+        console.log('result from OCTOKIT:', result.data.login);
+        const githubUsername = `github_${results.data.login}`;
+        // BETTER WAY WITH OBJECT STRUCTURING(??)
+        res.locals.githubUsername = githubUsername;
+
+
+        // query db to see if account exists
+        db.query(query, [githubUsername])
+          .then((result) => {
+            // if user doesn't exist, pass them along to createUser
+            if (result.rows.length === 0) {
+              return userController.createUser(req, res, next)
+            } else {
+                console.log('check password');
+                if (result.rows[0].password === password) {
+                  // insert logic for randomized, more secure ssid
+                  res.locals.id = result.rows[0].id;
+                  return next();
+                } else {
+                  res.redirect('/signup');
+                }
+              }
+          })
+          .catch((err) => {
+            return next({
+              log: 'Express error handler caught unknown middleware error',
+              status: 400,
+              message: {
+                err: 'error in userController.verifyUser - login credentials incorrect',
+              },
+            });
+          });
+        // if account does not yet exist in db --> send to createUser middleware? 
+
+        // if account does exists --> send to verify user middleware, then 
       })
       
 
@@ -129,7 +175,6 @@ userController.verifyUser = (req, res, next) => {
       // add entry to users table where username = github username or github email
       // attach username to res.locals nad invoke next middleware 
 
-    // invoke next middleware
   };
 
 module.exports = userController;
